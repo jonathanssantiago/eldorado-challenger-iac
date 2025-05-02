790d8fd1c0f426b546dc1621a1067858c292e35d
Jonathan Santiago <jonathan.santiago@creathus.org.br>
2025-05-02 18:46:50 -0400

    feat: add S3 bucket policy for public read access

--- infra/s3-bucket-static/main.tf
+++ infra/s3-bucket-static/main.tf
@@ -49,3 +49,20 @@ resource "aws_s3_bucket_acl" "static_site_bucket" {
   bucket = aws_s3_bucket.static_site_bucket.id
   acl    = "public-read"
 }
+
+resource "aws_s3_bucket_policy" "static_site_bucket" {
+  bucket = aws_s3_bucket.static_site_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.static_site_bucket.arn}/*"
+      }
+    ]
+  })
+}
